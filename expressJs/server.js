1c572cdea8e243c8197405ba9b14d41d8e10651c
KYUNG TAE <kkobe1228@gmail.com>
2025-08-01 18:35:34 +0900

    commit

--- expressJs/server.js
+++ expressJs/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRouter = require('./routes/users.router.js');
 const postsRouter = require('./routes/posts.router.js');
+const path = require('path');
 
 // constants
 const PORT = 4000;
@@ -9,7 +10,14 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+// application setting
+// 특정 엔진을 템플릿 엔진으로 사용하기 위한 설정
+app.set('view engine', 'hbs');
+// view 파일들이 모여있는 폴더를 명시
+app.set('views', path.join(__dirname, 'views'));
+
 // middleware setting
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use((req, res, next) => {
     const start = Date.now();
@@ -20,9 +28,14 @@ app.use((req, res, next) => {
 });
 
 // home
+// app.get('/', (req, res) => {
+//     res.send("Hello, World!");
+// }); 
 app.get('/', (req, res) => {
-    res.send("Hello, World!");
-});
+    res.render('index', {
+        imageTitle: "It is a cuty doggy. (using template engine)"
+    })
+})
 
 // route setting
 app.use('/users', usersRouter);
